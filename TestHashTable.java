0b1c6c6bc61e0b102adca56a3072170f8247c359
7rutasan <7rutasan@gmail.com>
2024-05-16 03:37:45 +0500

    MyHashTable class

---
+++ TestHashTable.java
@@ -0,0 +1,200 @@
+import java.util.Objects;
+import java.util.Random;
+
+class MyHashTable<K, V> {
+
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public MyHashTable() {
+        chainArray = (HashNode<K, V>[]) new HashNode[M];
+        size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public MyHashTable(int M) {
+        this.M = M;
+        chainArray = (HashNode<K, V>[]) new HashNode[M];
+        size = 0;
+    }
+
+    private int hash(K key) {
+        return Math.abs(Objects.hashCode(key) % M);
+    }
+
+    public void put(K key, V value) {
+        int bucketIndex = hash(key);
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        HashNode<K, V> head = chainArray[bucketIndex];
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+
+        size++;
+        newNode.next = chainArray[bucketIndex];
+        chainArray[bucketIndex] = newNode;
+    }
+
+    public V get(K key) {
+        int bucketIndex = hash(key);
+        HashNode<K, V> head = chainArray[bucketIndex];
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                return head.value;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int bucketIndex = hash(key);
+        HashNode<K, V> head = chainArray[bucketIndex];
+        HashNode<K, V> prev = null;
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                if (prev != null) {
+                    prev.next = head.next;
+                } else {
+                    chainArray[bucketIndex] = head.next;
+                }
+                size--;
+                return head.value;
+            }
+            prev = head;
+            head = head.next;
+        }
+        return null;
+    }
+
+    public boolean contains(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> head = chainArray[i];
+            while (head != null) {
+                if (head.value.equals(value)) {
+                    return true;
+                }
+                head = head.next;
+            }
+        }
+        return false;
+    }
+
+    public K getKey(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> head = chainArray[i];
+            while (head != null) {
+                if (head.value.equals(value)) {
+                    return head.key;
+                }
+                head = head.next;
+            }
+        }
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printBucketSizes() {
+        for (int i = 0; i < M; i++) {
+            int count = 0;
+            HashNode<K, V> head = chainArray[i];
+            while (head != null) {
+                count++;
+                head = head.next;
+            }
+            System.out.println("Bucket " + i + ": " + count + " elements");
+        }
+    }
+}
+
+class MyTestingClass {
+    private int id;
+    private String name;
+
+    public MyTestingClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + (name == null ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MyTestingClass other = (MyTestingClass) obj;
+        return id == other.id && Objects.equals(name, other.name);
+    }
+
+    @Override
+    public String toString() {
+        return "MyTestingClass{id=" + id + ", name='" + name + "'}";
+    }
+}
+
+class Student {
+    private int rollNumber;
+    private String name;
+
+    public Student(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{rollNumber=" + rollNumber + ", name='" + name + "'}";
+    }
+}
+
+public class TestHashTable {
+    public static void main(String[] args) {
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>();
+
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int id = random.nextInt(100000);
+            String name = "Name" + id;
+            MyTestingClass key = new MyTestingClass(id, name);
+            Student value = new Student(id, name);
+            table.put(key, value);
+        }
+
+        table.printBucketSizes();
+    }
+}
